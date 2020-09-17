2ac17c9888f1f1ade1d562c96055decf9d3a84b6
MCookson2 <matt.cookson2@gmail.com>
2020-09-17 17:17:01 +0100

    Change some CSS to make it look better, add reset button to clear previous FizzBuzz, fix bug so you can reset during calculation so you don't have to keep pressing it and wait for it to be over.

--- script.js
+++ script.js
@@ -1,6 +1,9 @@
 const numb = document.getElementById('n')
 const output = document.getElementById('output')
 const calc = document.getElementById('calc')
+const resetButton = document.getElementById('reset')
+
+let aborted = false;
 
 function timer(ms) {
     return new Promise(res => setTimeout(res, ms));
@@ -8,6 +11,7 @@ function timer(ms) {
 
 // had to make function async to add delay after every iteration of for loop
 async function fizzBuzz() {
+    aborted = false;
     let result;
     for (let i = 1; i <= numb.value; i++) {
             if (i % 15 === 0) {
@@ -22,9 +26,22 @@ async function fizzBuzz() {
             var node = document.createElement('p')
             var text = document.createTextNode(result)
             node.appendChild(text)
+            node.classList.add('deleted')
             output.appendChild(node)
-            await timer(500);
+            await timer(200);
+            node.classList.add('transitioned')
+            if (aborted === true) {
+                break;
+            }
         }
 }
 
 calc.addEventListener('click', fizzBuzz)
+
+resetButton.addEventListener('click', function(){
+    const deleteable = document.querySelectorAll('p.deleted')
+    deleteable.forEach(p => {
+        aborted = true;
+        p.parentNode.removeChild(p)
+    })
+})
