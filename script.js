54de142bb6174a797175b03e1e9f4285063a2e05
MCookson2 <matt.cookson2@gmail.com>
2020-09-21 21:22:32 +0100

    fix bugs, add auto scroll on calc button hit.  Add back to top button

--- script.js
+++ script.js
@@ -2,8 +2,14 @@ const numb = document.getElementById('n')
 const output = document.getElementById('output')
 const calc = document.getElementById('calc')
 const resetButton = document.getElementById('reset')
+const topButton = document.getElementById('top')
 
 let aborted = false;
+let computed = false;
+
+function bottom() {
+    document.getElementById('bottom').scrollIntoView();
+}
 
 function timer(ms) {
     return new Promise(res => setTimeout(res, ms));
@@ -13,6 +19,15 @@ function timer(ms) {
 async function fizzBuzz() {
     aborted = false;
     let result;
+
+    if (computed === true) {
+        const deleteable = document.querySelectorAll('p.deleted')
+        deleteable.forEach(p => {
+            computed = false;
+            p.parentNode.removeChild(p)
+    })
+    }
+
     for (let i = 1; i <= numb.value; i++) {
             if (i % 15 === 0) {
                 result = 'FizzBuzz'
@@ -23,25 +38,40 @@ async function fizzBuzz() {
             } else {
                 result = i
             }
+
             var node = document.createElement('p')
             var text = document.createTextNode(result)
+
             node.appendChild(text)
             node.classList.add('deleted')
             output.appendChild(node)
-            await timer(200);
+            await timer(100);
             node.classList.add('transitioned')
+            computed = true;
+
+            bottom();
+
             if (aborted === true) {
                 break;
             }
         }
+    if (numb.value >= 20) {
+        topButton.style.display = 'block';
+        bottom();
+    }   
 }
 
 calc.addEventListener('click', fizzBuzz)
 
 resetButton.addEventListener('click', function(){
+    numb.value = '';
     const deleteable = document.querySelectorAll('p.deleted')
     deleteable.forEach(p => {
         aborted = true;
         p.parentNode.removeChild(p)
     })
+})
+
+topButton.addEventListener('click', function(){
+    document.documentElement.scrollTop = 0;
 })
